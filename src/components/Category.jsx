7932ad658b2357c6f32e26d9e6af63c5239c4efa
Giovanny Vargas <yovaz05@gmail.com>
2023-05-24 19:28:29 -0500

    clase 24-05-2023

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -3,21 +3,24 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import CATEGORIES from '../data/Categories';
 
 const Category = () => {
 	return (
 		<FormControl fullWidth>
-			<InputLabel id='demo-simple-select-label'>Age</InputLabel>
+			<InputLabel id='demo-simple-select-label'>Category</InputLabel>
 			<Select
 				labelId='demo-simple-select-label'
 				id='demo-simple-select'
 				value={age}
-				label='Age'
+				label='Category'
 				onChange={handleChange}
 			>
-				<MenuItem value={10}>Ten</MenuItem>
-				<MenuItem value={20}>Twenty</MenuItem>
-				<MenuItem value={30}>Thirty</MenuItem>
+				{CATEGORIES.map(category => (
+					<MenuItem key={category.value} value={category.value}>
+						{category.label}
+					</MenuItem>
+				))}
 			</Select>
 		</FormControl>
 	);
